a243506dea11306db3cd08aa5fd36e70617b5548
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2023-01-09 03:43:18 +0300

    Login form functionality

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,18 +1,37 @@
+import React, {ChangeEvent, FormEvent} from "react";
+
+interface LoginProps{
+    login: string;
+    password: string
+}
 export const LoginForm = () => {
+    const [credentials, setCredentials] = React.useState<LoginProps>({login:'', password:''});
+
+
+    const handleChange = (event:  ChangeEvent<HTMLInputElement>) => {
+        setCredentials({...credentials, [event.currentTarget.name]: event.currentTarget.value})
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        localStorage.setItem('login', credentials.login)
+        localStorage.setItem('password', credentials.password)
+    }
+
     return (
         <div className="login-form">
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="label">
                 <label htmlFor="login">Login</label>
             </div>
             <div className="input">
-                <input id="login" type="text" placeholder='Login'/>
+                <input name='login' id="login" type="text" placeholder='Login' required value={credentials.login} onChange={handleChange}/>
             </div>
             <div className="label">
                 <label htmlFor="password">Password</label>
             </div>
             <div className="input">
-                <input id="password" type="text" placeholder='Password'/>
+                <input name='password' id="password" type="text" placeholder='Password' required value={credentials.password} onChange={handleChange}/>
             </div>
           <div className="button">
               <button type="submit">Login</button>
