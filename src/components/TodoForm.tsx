ec13a2c4912f5e5170fa03931a8ecda157e11ec6
Stephen Kilonzo <stephenkilonzo22@yahoo.com>
2023-01-09 13:43:08 +0300

    todo local storage

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,14 +1,27 @@
-import React, {FormEvent} from "react";
+import React, {ChangeEvent, FormEvent} from "react";
 
 export const TodoForm: React.FC =()=> {
 
+    const [todo, setTodo] = React.useState<string>('')
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) =>{
+        setTodo(event.currentTarget.value)
+    }
+
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        console.log("todo added")
+        const storedTodos = localStorage.getItem('todos')
+        if(!storedTodos){
+            localStorage.setItem('todos', JSON.stringify([todo]))
+        }else{
+            localStorage.setItem('todos', JSON.stringify([...JSON.parse(storedTodos), todo]))
+        }
+        setTodo('')
     }
+
     return (<div className="todo-form">
         <form onSubmit={handleSubmit}>
-            <input className='todo' type="text" required name="todo" placeholder="Type your todos..."/>
+            <input className='todo' value={todo} onChange={handleChange} type="text" required name="todo" placeholder="Type your todos..."/>
             <button type="submit">Add Todo</button>
         </form>
     </div>)
